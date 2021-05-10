6e252b3450de141e071407516a28cda6bd8e5b19
Martin Douša <martindousa186@gmail.com>
2021-05-11 01:15:12 +0200

    refactoring

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -2,10 +2,13 @@
 #include "renderer/basic_renderer.h"
 #include "memory/heap.h"
 #include "scheduling/pit/pit.h"
+#include "utils/performance.h"
 
 extern "C" void _start(BootInfo *bootInfo)
 {
   InitializeKernel(bootInfo);
+
+  ShowStats(1700, 50);
 
   while (true)
   {
